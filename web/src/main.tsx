e93f0547a83dade0696dbc04d48a0f04e90482ec
tyler-at-cast <tyler@cast.ai>
2026-02-23 09:20:12 -0500

    chore(03-03): remove schedule page routes
    - Remove ScheduleNewPage and ScheduleEditPage imports
    - Remove /schedules/new and /schedules/:id routes
    - Page files preserved for future reference
    
    Files modified:
    - web/src/main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,8 +7,6 @@ import CloudAccountsPage from './pages/CloudAccountsPage'
 import InstancesPage from './pages/InstancesPage'
 import InstanceDetailPage from './pages/InstanceDetailPage'
 import SchedulesPage from './pages/SchedulesPage'
-import ScheduleNewPage from './pages/ScheduleNewPage'
-import ScheduleEditPage from './pages/ScheduleEditPage'
 import RecommendationsPage from './pages/RecommendationsPage'
 import AuditLogPage from './pages/AuditLogPage'
 import './index.css'
@@ -39,8 +37,6 @@ root.render(
         <Route path="instances" element={<InstancesPage />} />
         <Route path="instances/:id" element={<InstanceDetailPage />} />
         <Route path="schedules" element={<SchedulesPage />} />
-        <Route path="schedules/new" element={<ScheduleNewPage />} />
-        <Route path="schedules/:id" element={<ScheduleEditPage />} />
         <Route path="recommendations" element={<RecommendationsPage />} />
         <Route path="audit-log" element={<AuditLogPage />} />
       </Route>
